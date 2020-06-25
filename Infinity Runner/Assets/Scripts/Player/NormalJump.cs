0dd70045ccf85ead9b9cf0d7c1a029d74e47f24a
jpsecundino <jpsecundino@users.noreply.github.com>
2020-06-25 03:30:27 -0300

    Powerups + Screen Clamp

---
+++ Infinity Runner/Assets/Scripts/Player/NormalJump.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalJump : MonoBehaviour
+{
+
+    public PlayerControl player;
+    public Rigidbody2D playerRb;
+    public float fallMultiplier = 5f;
+    public float smallJumpMultiplier = 10f;
+
+
+    void Awake()
+    {
+        player = GetComponent<PlayerControl>();
+        playerRb = player.GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (player.IsGrounded())
+            {
+                playerRb.velocity = new Vector2(playerRb.velocity.x, player.jumpForce);
+            }
+            //playerRb.velocity = new Vector2(playerRb.velocity.x, jumpForce);
+        }
+
+        bool _falling = playerRb.velocity.y < 0;
+        bool _jumping = Input.GetButton("Jump");
+
+        if (_falling)//faster fall
+        {
+            playerRb.velocity += Vector2.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+
+        }
+        else if (!_falling && !_jumping)//small jump
+        {
+            playerRb.velocity += Vector2.up * Physics.gravity.y * (smallJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+    }
+}
